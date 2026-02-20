550676fd008bd5c9a5df194bb6b5919816e3caf3
Binh Minh <bmmit.work@gmail.com>
2026-02-21 00:01:45 +0700

    10843 - Anne's game

---
+++ 10843_AnnesGame.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int unsigned long long
+#define endl "\n"
+
+#define IF(cond, t, f) (cond ? t : f)
+#define OFILE(finp, fout) freopen(finp, "r", stdin), freopen(fout, "w", stdout)
+#define FAST_IO ios_base::sync_with_stdio(false), (void)cin.tie()
+#define MULTI \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p);
+
+#define P(T, O, C)                          \
+    template <class U>                      \
+    ostream &operator<<(ostream &o, T<U> v) \
+    {                                       \
+        o << O;                             \
+        int f = 0;                          \
+        for (auto &i : v)                   \
+            o << (f++ ? ", " : "") << i;    \
+        return o << C;                      \
+    }
+
+#define Q(T, O, C)                             \
+    template <class K, class V>                \
+    ostream &operator<<(ostream &o, T<K, V> v) \
+    {                                          \
+        o << O;                                \
+        int f = 0;                             \
+        for (auto &i : v)                      \
+            o << (f++ ? ", " : "") << i;       \
+        return o << C;                         \
+    }
+
+P(vector, '[', ']')
+P(set, '{', '}')
+Q(map, '{', '}')
+
+template <class T, class V>
+ostream &operator<<(ostream &o, pair<T, V> p)
+{
+    return o << "(" << p.first << ", " << p.second << ")";
+}
+
+template <class T, size_t N>
+ostream &operator<<(ostream &o, array<T, N> v)
+{
+    o << "[";
+    int f = 0;
+    for (auto &i : v)
+        o << (f++ ? ", " : "") << i;
+    return o << "]";
+}
+
+void dbg(const char *s) { cerr << endl; }
+
+template <class T, class... A>
+void dbg(const char *s, T v, A... a)
+{
+    const char *c = s;
+    int b = 0;
+    while (*c && (*c != ',' || b))
+        b += bool(strchr("({[", *c)), b -= bool(strchr(")}]", *c)), c++;
+    cerr.write(s, c - s) << " = " << v << (sizeof...(a) ? " |" : "");
+    if (sizeof...(a))
+        dbg(c + 1, a...);
+    else
+        cerr << endl;
+}
+
+#ifdef LOCAL
+#define debug(...) dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+#define MOD 2000000011
+
+int pow(int a, int b)
+{
+    if (!b)
+        return 1;
+    int half = pow(a, b / 2);
+    return half * half % MOD * IF(b & 1, a, 1) % MOD;
+}
+
+int caseCnt = 1;
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << "Case #" << caseCnt++ << ": " << IF(n < 2, 1, pow(n, n - 2)) << endl;
+}
+
+signed main()
+{
+    FAST_IO;
+
+    MULTI
+    solve();
+
+    return 0;
+}
